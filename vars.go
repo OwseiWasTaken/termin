70a538b41864e4c666e143a463333abf032792a6
owseiwastaken <owseiwastaken@gmail.com>
2022-09-18 21:58:00 -0300

    stdin on Win

--- vars.go
+++ vars.go
@@ -2,7 +2,7 @@
 var (
 	termy, termx = GetTerminalSize()
 	wins = []*Window{}
-	Win = MakeWin("Main", stdout, 0, termy, 0, termx)
+	Win = MakeWin("Main", stdout, stdin, 0, termy, 0, termx)
 	nw = &Window{} // NoWin (used in end())
 	lk []byte
 )
