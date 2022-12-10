3960d50acf06852d353b9a1e849b33339eca40b0
owsei <owseiwastaken@gmail.com>
2022-12-10 10:28:43 -0300

    e/writekey explain what's happening

--- examples/writekey.go
+++ examples/writekey.go
@@ -6,7 +6,8 @@ func main(){
 	InitTermin()
 	var k string
 	for ;k != "pause/break";{
-		k = wgtk(Win)
+		k = wgtk(Win) // wait for keyboard key
+		// wgtk sets lk (last key []byte) and returns the key's name
 		wwrite(Win, spf("%v:%s\n", lk, k))
 	}
 	StopTermin()
